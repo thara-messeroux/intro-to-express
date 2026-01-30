be7087a3bbf2dc673fa93107277a12bff41224d6
thara-messeroux <tharamesseroux@gmail.com>
2026-01-29 21:29:27 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// Import Express
+const express = require('express')
+
+// Create an Express app
+const app = express()
+
+
+// Routes (urls)
+console.log("Test")
+console.log("Test")
+console.log("Test")
+console.log("Test")
+console.log("Test")
+console.log("Test2")
+
+
+// Define routes here (we'll add them soon)
+
+// Listen
+
+// Listen for requests on port 3000
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
+
+
+
+
